7b1d4ac8d398d61c8f3f47e421123e725be1af82
Vladimir <riuson@gmail.com>
2015-08-30 03:32:46 +0500

    Added menu control

---
+++ BugTracker.Core/Controls/MenuPanel.cs
@@ -0,0 +1,21 @@
+﻿using BugTracker.Core.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BugTracker.Core.Controls
+{
+    public class MenuPanel : FlowLayoutPanel
+    {
+        public MenuPanel()
+        {
+        }
+
+        public void Add(Button button)
+        {
+            this.Controls.Add(button);
+        }
+    }
+}
