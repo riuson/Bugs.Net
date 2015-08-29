31111965624fa7f5d459325d4e402f35c97b5b0c
Vladimir <riuson@gmail.com>
2015-08-29 12:51:23 +0500

    Added models to database

---
+++ BugTracker.DB/Models/Enums/Status.cs
@@ -0,0 +1,14 @@
+﻿using BugTracker.DB.Interfaces;
+using FluentNHibernate.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BugTracker.DB.Models
+{
+    public class Status : Entity, IVocabulary
+    {
+        public virtual string Value { get; set; }
+    }
+}
