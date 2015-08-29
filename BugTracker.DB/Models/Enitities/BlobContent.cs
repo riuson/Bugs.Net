fd69502da20f2352f176b38acc3aa6fe890d58c1
Vladimir <riuson@gmail.com>
2015-08-29 22:08:56 +0500

    Added methods for BlobContent to read/write stream

--- BugTracker.DB/Models/Enitities/BlobContent.cs
+++ BugTracker.DB/Models/Enitities/BlobContent.cs
@@ -1,6 +1,7 @@
 ﻿using FluentNHibernate.Data;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -9,5 +10,22 @@ namespace BugTracker.DB.Models
     public class BlobContent : Entity
     {
         public virtual byte[] Content { get; set; }
+
+        public virtual void ReadFrom(Stream stream)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                stream.CopyTo(ms);
+                this.Content = ms.ToArray();
+            }
+        }
+
+        public virtual void WriteTo(Stream stream)
+        {
+            using (MemoryStream ms = new MemoryStream(this.Content))
+            {
+                ms.CopyTo(stream);
+            }
+        }
     }
 }
