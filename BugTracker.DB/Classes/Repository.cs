aed09f2ea5b0a3eccbf6b40cd8f0ecc00817c316
Vladimir <riuson@gmail.com>
2015-08-31 21:12:15 +0500

    Added generic repository and one testing

---
+++ BugTracker.DB/Classes/Repository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BugTracker.DB.Classes
+{
+    public abstract class Repository<T> where T : new()
+    {
+        protected NHibernate.ISession Session { get; set; }
+
+        public Repository(BugTracker.DB.Interfaces.ISession session)
+        {
+            Session s = session as Session;
+            this.Session = s.NHSession;
+        }
+
+        public T Load(object id)
+        {
+            return this.Session.Load<T>(id);
+        }
+
+        public T GetById(object id)
+        {
+            return this.Session.Get<T>(id);
+        }
+
+        public ICollection<T> List()
+        {
+            return this.Session.CreateCriteria(typeof(T)).List<T>();
+        }
+
+        public void Save(T entity)
+        {
+            this.Session.Save(entity);
+        }
+
+        public void SaveOrUpdate(T entity)
+        {
+            this.Session.SaveOrUpdate(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            this.Session.Delete(entity);
+        }
+    }
+}
--- BugTracker.DB/Classes/Session.cs
+++ BugTracker.DB/Classes/Session.cs
@@ -8,16 +8,19 @@ namespace BugTracker.DB.Classes
 {
     internal class Session : BugTracker.DB.Interfaces.ISession
     {
-        private NHibernate.ISession mSession;
+        public NHibernate.ISession NHSession { get; private set; }
+        public NHibernate.ITransaction NHTransaction { get; private set; }
 
         public Session(NHibernate.ISession session)
         {
-            this.mSession = session;
+            this.NHSession = session;
+            this.NHTransaction = this.NHSession.BeginTransaction();
         }
 
         public void Dispose()
         {
-            this.mSession.Dispose();
+            this.NHTransaction.Commit();
+            this.NHSession.Dispose();
         }
     }
 }
