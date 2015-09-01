0819a416b3e15cb647d77f28c345114237541a0e
Vladimir <riuson@gmail.com>
2015-09-01 20:53:14 +0500

    Added project

---
+++ BugTracker.Projects/Classes/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BugTracker.Core.Classes;
+using BugTracker.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BugTracker.Projects.Classes
+{
+    internal class Plugin : IPlugin
+    {
+        private IApplication mApp;
+
+        public void Initialize(IApplication app)
+        {
+            this.mApp = app;
+        }
+
+        public IButton[] GetCommandLinks(string tag)
+        {
+            switch (tag)
+            {
+                case "startpage":
+                    {
+                        IButton menuItemProjects = MenuPanelFabric.CreateMenuItem("Projects", "Manage projects");
+                        menuItemProjects.Click += delegate(object sender, EventArgs ea)
+                        {
+                        };
+
+                        return new IButton[] { menuItemProjects };
+                    }
+                default:
+                    return new IButton[] { };
+            }
+        }
+    }
+}
