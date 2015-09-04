1c7374b83c8fae347619d3764f5ba12682b44595
Vladimir <riuson@gmail.com>
2015-09-05 00:19:43 +0500

    Added generic vocabulary editor control

--- BugTracker.Vocabulary/Classes/Plugin.cs
+++ BugTracker.Vocabulary/Classes/Plugin.cs
@@ -1,5 +1,7 @@
 ﻿using BugTracker.Core.Classes;
 using BugTracker.Core.Interfaces;
+using BugTracker.DB.Entities;
+using BugTracker.Vocabulary.Controls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,21 +25,59 @@ namespace BugTracker.Vocabulary.Classes
             {
                 case "settings":
                     {
-                        IButton menuItemSettings = MenuPanelFabric.CreateMenuItem("Vocabulary", "Configure database");
-                        menuItemSettings.Click += delegate(object sender, EventArgs ea)
+                        IButton menuItemPriorityList = MenuPanelFabric.CreateMenuItem("Priority list", "Vocabulary editor");
+                        menuItemPriorityList.Click += delegate(object sender, EventArgs ea)
                         {
-                            this.ShowVocabulary();
+                            this.ShowPriorityList();
                         };
 
-                        return new IButton[] { menuItemSettings };
+                        IButton menuItemProblemTypeList = MenuPanelFabric.CreateMenuItem("Problem Type list", "Vocabulary editor");
+                        menuItemProblemTypeList.Click += delegate(object sender, EventArgs ea)
+                        {
+                            this.ShowProblemTypeList();
+                        };
+
+                        IButton menuItemSolutionList = MenuPanelFabric.CreateMenuItem("Solution list", "Vocabulary editor");
+                        menuItemSolutionList.Click += delegate(object sender, EventArgs ea)
+                        {
+                            this.ShowSolutionList();
+                        };
+
+                        IButton menuItemStatusList = MenuPanelFabric.CreateMenuItem("Status list", "Vocabulary editor");
+                        menuItemStatusList.Click += delegate(object sender, EventArgs ea)
+                        {
+                            this.ShowStatusList();
+                        };
+
+                        return new IButton[] { menuItemPriorityList, menuItemProblemTypeList, menuItemSolutionList, menuItemStatusList };
                     }
                 default:
                     return new IButton[] { };
             }
         }
 
-        private void ShowVocabulary()
+        private void ShowPriorityList()
         {
+            ControlVocabularyList<Priority> controlEditor = new ControlVocabularyList<Priority>(this.mApp);
+            this.mApp.Controls.Show(controlEditor);
+        }
+
+        private void ShowProblemTypeList()
+        {
+            ControlVocabularyList<ProblemType> controlEditor = new ControlVocabularyList<ProblemType>(this.mApp);
+            this.mApp.Controls.Show(controlEditor);
+        }
+
+        private void ShowSolutionList()
+        {
+            ControlVocabularyList<Solution> controlEditor = new ControlVocabularyList<Solution>(this.mApp);
+            this.mApp.Controls.Show(controlEditor);
+        }
+
+        private void ShowStatusList()
+        {
+            ControlVocabularyList<Status> controlEditor = new ControlVocabularyList<Status>(this.mApp);
+            this.mApp.Controls.Show(controlEditor);
         }
     }
 }
