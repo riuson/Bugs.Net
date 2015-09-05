a33a32c2b0c74d7dbb9cdee75cadd594e7610bad
Vladimir <riuson@gmail.com>
2015-09-05 23:03:24 +0500

    Added ticket project

---
+++ BugTracker.Ticket/Classes/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using BugTracker.Core.Interfaces;
+using BugTracker.Tickets.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Ticket.Classes
+{
+    internal class Plugin : IPlugin
+    {
+        private IApplication mApp;
+
+        public void Initialize(IApplication app)
+        {
+            this.mApp = app;
+
+            this.mApp.Messages.Subscribe(typeof(AddTicketEventArgs), this.AddTicketList);
+            this.mApp.Messages.Subscribe(typeof(EditTicketEventArgs), this.EditTicketList);
+        }
+
+        public IButton[] GetCommandLinks(string tag)
+        {
+            return new IButton[] { };
+        }
+
+        private void AddTicketList(object sender, MessageEventArgs e)
+        {
+        }
+
+        private void EditTicketList(object sender, MessageEventArgs e)
+        {
+        }
+    }
+}
