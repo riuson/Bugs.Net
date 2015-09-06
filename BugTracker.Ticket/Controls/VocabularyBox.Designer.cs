6ec1657c1a0a5430181b3fba52eef865bbef786b
Vladimir <riuson@gmail.com>
2015-09-06 13:19:17 +0500

    Added control for vocabulary item select

---
+++ BugTracker.Ticket/Controls/VocabularyBox.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace BugTracker.Ticket.Controls
+{
+    partial class VocabularyBox<T>
+    {
+        /// <summary> 
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Обязательный метод для поддержки конструктора - не изменяйте 
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(355, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.linkLabel1.Location = new System.Drawing.Point(355, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(16, 13);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "...";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // VocabularyEdit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Name = "VocabularyEdit";
+            this.Size = new System.Drawing.Size(371, 21);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
---
+++ BugTracker.Ticket/Controls/VocabularyBox.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BugTracker.DB.Interfaces;
+using BugTracker.DB;
+using BugTracker.DB.Classes;
+using BugTracker.DB.Entities;
+using BugTracker.Core.Interfaces;
+using BugTracker.Vocabulary.Events;
+
+namespace BugTracker.Ticket.Controls
+{
+    public partial class VocabularyBox<T> : UserControl where T : new()
+    {
+        private ICollection<T> mEntityList;
+        private IEnumerable<DisplayData> mDisplayList;
+        private BindingSource mBS;
+        private IApplication mApp;
+
+        public VocabularyBox(IApplication app)
+        {
+            InitializeComponent();
+            this.mApp = app;
+            this.mBS = new BindingSource();
+            this.mBS.AllowNew = true;
+            
+            this.comboBox1.DataSource = this.mBS;
+            this.UpdateList();
+        }
+
+        private void UpdateList()
+        {
+            using (ISession session = SessionManager.Instance.OpenSession())
+            {
+                Repository<T> repository = new Repository<T>(session);
+                this.mEntityList = repository.List();
+                this.mDisplayList = this.mEntityList.Select<T, DisplayData>(e => new DisplayData(e));
+                this.mBS.DataSource = this.mDisplayList;
+            }
+        }
+
+        public T SelectedValue
+        {
+            get
+            {
+                return default(T);
+            }
+            set
+            {
+
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            this.MinimumSize = new Size(0, this.comboBox1.Height);
+            this.MaximumSize = new Size(0, this.comboBox1.Height);
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+
+            base.OnLoad(e);
+        }
+
+        private class DisplayData
+        {
+            public T Value { get; private set; }
+
+            public DisplayData(T value)
+            {
+                this.Value = value;
+            }
+
+            public override string ToString()
+            {
+                IVocabulary vocabulary = (IVocabulary)this.Value;
+                return vocabulary.Value;
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+        }
+    }
+}
