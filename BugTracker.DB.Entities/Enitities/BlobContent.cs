f5c38ebd258d7688a706fff58c40d9a2485a81cd
Vladimir <riuson@gmail.com>
2015-09-13 12:21:21 +0500

    Added methods for read/write string from/to BlobContent.Content

--- BugTracker.DB.Entities/Enitities/BlobContent.cs
+++ BugTracker.DB.Entities/Enitities/BlobContent.cs
@@ -10,6 +10,10 @@ namespace BugTracker.DB.Entities
     {
         public virtual byte[] Content { get; set; }
 
+        /// <summary>
+        /// Set Content from stream
+        /// </summary>
+        /// <param name="stream">Source stream</param>
         public virtual void ReadFrom(Stream stream)
         {
             using (MemoryStream ms = new MemoryStream())
@@ -19,12 +23,34 @@ namespace BugTracker.DB.Entities
             }
         }
 
+        /// <summary>
+        /// Set Content from value
+        /// </summary>
+        /// <param name="value">Source string</param>
+        public virtual void SetString(string value)
+        {
+            this.Content = Encoding.UTF8.GetBytes(value);
+        }
+
+        /// <summary>
+        /// Write Content to output stream
+        /// </summary>
+        /// <param name="stream">Output stream</param>
         public virtual void WriteTo(Stream stream)
         {
             using (MemoryStream ms = new MemoryStream(this.Content))
             {
                 ms.CopyTo(stream);
             }
+        }
+
+        /// <summary>
+        /// Get Content as string
+        /// </summary>
+        /// <returns>Content string</returns>
+        public virtual string GetString()
+        {
+            return Encoding.UTF8.GetString(this.Content);
         }
     }
 }
