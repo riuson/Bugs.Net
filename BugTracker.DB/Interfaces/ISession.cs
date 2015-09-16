ad4e36a5619421f0a26054a3e8ff08228a348f32
Vladimir <riuson@gmail.com>
2015-09-16 18:03:31 +0500

    Added comments to interfaces

--- BugTracker.DB/Interfaces/ISession.cs
+++ BugTracker.DB/Interfaces/ISession.cs
@@ -7,6 +7,9 @@ namespace BugTracker.DB.Interfaces
 {
     public interface ISession : IDisposable
     {
+        /// <summary>
+        /// Transaction in session
+        /// </summary>
         ITransaction Transaction { get; }
     }
 }
--- BugTracker.DB/Interfaces/ITransaction.cs
+++ BugTracker.DB/Interfaces/ITransaction.cs
@@ -7,7 +7,14 @@ namespace BugTracker.DB.Interfaces
 {
     public interface ITransaction
     {
+        /// <summary>
+        /// Commit transaction on session dispose
+        /// </summary>
         void Commit();
+
+        /// <summary>
+        /// Rollback transaction on session dispose
+        /// </summary>
         void Rollback();
     }
 }
