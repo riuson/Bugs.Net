0ed33aa10074e783e49a1dea4a86ae16fadbd5d2
Vladimir <riuson@gmail.com>
2015-09-17 20:02:11 +0500

    Added test for MessageCenterTest

---
+++ BugTracker.Core.Tests/Classes/MessageCenterTest.cs
@@ -0,0 +1,179 @@
+﻿using BugTracker.Core.Classes;
+using BugTracker.Core.Interfaces;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Core.Tests.Classes
+{
+    [TestFixture]
+    internal class MessageCenterTest
+    {
+        private int mValue1;
+        private int mValue2;
+
+        public MessageCenterTest()
+        {
+            this.mValue1 = 0;
+            this.mValue2 = 0;
+        }
+
+        [Test]
+        public void CanCreate()
+        {
+            Assert.That(
+                delegate()
+                {
+                    MessageCenter mc = new MessageCenter();
+                },
+                Throws.Nothing
+            );
+        }
+
+        [Test]
+        public void CanSend()
+        {
+            Assert.That(
+                delegate()
+                {
+                    MessageCenter mc = new MessageCenter();
+                    mc.Send(this, new MessageEventArgs());
+                },
+                Throws.Nothing
+            );
+        }
+
+        [Test]
+        public void CanSubscribe()
+        {
+            MessageCenter mc = new MessageCenter();
+            mc.Subscribe(typeof(MessageEventArgs), this.Callback);
+
+            MessageEventArgs ea = new MessageEventArgs();
+            ea.Processed = false;
+
+            this.mValue1 = 0;
+            mc.Send(this, ea);
+
+            Assert.That(ea.Processed, Is.True);
+            Assert.That(this.mValue1, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void CanUnSubscribe()
+        {
+            MessageCenter mc = new MessageCenter();
+            mc.Subscribe(typeof(MessageEventArgs), this.Callback);
+
+            MessageEventArgs ea = new MessageEventArgs();
+            ea.Processed = false;
+
+            this.mValue1 = 0;
+            mc.Send(this, ea);
+
+            mc.Unsubscribe(typeof(MessageEventArgs), this.Callback);
+
+            ea.Processed = false;
+            this.mValue1 = 0;
+            mc.Send(this, ea);
+
+            Assert.That(ea.Processed, Is.False);
+            Assert.That(this.mValue1, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CanSubscribeInherited()
+        {
+            MessageCenter mc = new MessageCenter();
+            mc.Subscribe(typeof(MessageEventArgsInherited), this.CallbackInherited);
+
+            MessageEventArgsInherited ea = new MessageEventArgsInherited();
+            ea.Processed = false;
+            ea.Value = 0;
+
+            this.mValue2 = 0;
+            mc.Send(this, ea);
+
+            Assert.That(ea.Processed, Is.True);
+            Assert.That(this.mValue2, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void CanUnSubscribeInherited()
+        {
+            MessageCenter mc = new MessageCenter();
+            mc.Subscribe(typeof(MessageEventArgsInherited), this.CallbackInherited);
+
+            MessageEventArgsInherited ea = new MessageEventArgsInherited();
+            ea.Processed = false;
+
+            this.mValue2 = 0;
+            mc.Send(this, ea);
+
+            mc.Unsubscribe(typeof(MessageEventArgsInherited), this.CallbackInherited);
+
+            ea.Processed = false;
+            this.mValue2 = 0;
+            mc.Send(this, ea);
+
+            Assert.That(ea.Processed, Is.False);
+            Assert.That(this.mValue2, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void CanSelectCallback()
+        {
+            MessageCenter mc = new MessageCenter();
+            mc.Subscribe(typeof(MessageEventArgs), this.Callback);
+            mc.Subscribe(typeof(MessageEventArgsInherited), this.CallbackInherited);
+
+            MessageEventArgs ea = new MessageEventArgs();
+            ea.Processed = false;
+            
+            MessageEventArgsInherited ea2 = new MessageEventArgsInherited();
+            ea2.Processed = false;
+            ea2.Value = 0;
+
+            this.mValue1 = 0;
+            this.mValue2 = 0;
+            mc.Send(this, ea);
+
+            Assert.That(ea.Processed, Is.True);
+            Assert.That(this.mValue1, Is.EqualTo(1));
+            Assert.That(this.mValue2, Is.EqualTo(0));
+
+            this.mValue1 = 0;
+            this.mValue2 = 0;
+            mc.Send(this, ea2);
+
+            Assert.That(ea2.Processed, Is.True);
+            Assert.That(this.mValue1, Is.EqualTo(0));
+            Assert.That(this.mValue2, Is.EqualTo(2));
+        }
+
+        private void Callback(object sender, MessageEventArgs ea)
+        {
+            this.mValue1++;
+            ea.Processed = true;
+        }
+
+        private void CallbackInherited(object sender, MessageEventArgs ea)
+        {
+            this.mValue2 += 2;
+            ea.Processed = true;
+        }
+
+        private class MessageEventArgsInherited : MessageEventArgs
+        {
+            public int Value { get; set; }
+
+            public MessageEventArgsInherited()
+            {
+                this.Value = 31415926;
+            }
+        }
+    }
+}
