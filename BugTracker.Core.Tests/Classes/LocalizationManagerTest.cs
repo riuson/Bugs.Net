3d4c4e1a7cb78432e03c39af6ba07c517d3aff7d
Vladimir <riuson@gmail.com>
2015-09-20 13:16:24 +0500

    Added test for translations

---
+++ BugTracker.Core.Tests/Classes/LocalizationManagerTest.cs
@@ -0,0 +1,43 @@
+﻿using BugTracker.Core.Classes;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BugTracker.Core.Tests.Classes
+{
+    [TestFixture]
+    internal class LocalizationManagerTest
+    {
+        [Test]
+        public void CanTranslate()
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+            string str = LocalizationManager.Instance.GetTranslation(Assembly.GetExecutingAssembly(), MethodBase.GetCurrentMethod(), "Test");
+
+            Assert.That(str, Is.EqualTo("Test"));
+
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru");
+            LocalizationManager.Instance.SetTranslation(Assembly.GetExecutingAssembly(), MethodBase.GetCurrentMethod(), "Test", "Тест");
+
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru");
+            str = LocalizationManager.Instance.GetTranslation(Assembly.GetExecutingAssembly(), MethodBase.GetCurrentMethod(), "Test");
+            Assert.That(str, Is.EqualTo("Тест"));
+
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+            str = LocalizationManager.Instance.GetTranslation(Assembly.GetExecutingAssembly(), MethodBase.GetCurrentMethod(), "Test");
+            Assert.That(str, Is.EqualTo("Test"));
+
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru");
+            str = LocalizationManager.Instance.GetTranslation(Assembly.GetExecutingAssembly(), MethodBase.GetCurrentMethod(), "Test");
+            Assert.That(str, Is.EqualTo("Тест"));
+
+            //LocalizationManager.Instance.Save();
+        }
+    }
+}
--- BugTracker.Core/Classes/LocalizationManager.cs
+++ BugTracker.Core/Classes/LocalizationManager.cs
@@ -60,6 +60,19 @@ namespace BugTracker.Core.Classes
             return data.GetTranslation(id, methodName, value, comment);
         }
 
+        public void SetTranslation(Assembly assembly, MethodBase method, string value, string translation, string comment = "")
+        {
+            CultureInfo culture = Thread.CurrentThread.CurrentUICulture;
+
+            TranslationData data = this.GetData(assembly, culture);
+
+            string className = method.ReflectedType.Name;
+            string methodName = this.CleanString(className + "_" + method.Name);
+            string id = this.GetHash(methodName + value);
+
+            data.SetTranslation(id, methodName, value, translation, comment);
+        }
+
         public void Save()
         {
             foreach (var value in this.mTranslations.Values)
@@ -149,15 +162,16 @@ namespace BugTracker.Core.Classes
                 }
             }
 
-            internal string GetTranslation(string id, string methodName, string valueDefault, string comment)
+            internal string GetTranslation(string id, string methodName, string source, string comment)
             {
                 XmlNode nodeMethod = this.mDocument.DocumentElement.SelectSingleNode(
                     String.Format("message[id={0}]", id));
 
                 if (nodeMethod != null)
                 {
-                    XmlNode cdata = nodeMethod.SelectSingleNode("translated");
-                    return cdata.InnerText;
+                    XmlNode nodeTranslated = nodeMethod.SelectSingleNode("translated");
+                    XmlCDataSection cdata = (XmlCDataSection)nodeTranslated.FirstChild;
+                    return cdata.Value;
                 }
                 else
                 {
@@ -170,26 +184,74 @@ namespace BugTracker.Core.Classes
 
                     {
                         XmlNode nodeSource = this.GetNode(nodeMessage, "source", true);
-                        XmlNode cdata = this.mDocument.CreateCDataSection(valueDefault);
+                        XmlCDataSection cdata = this.mDocument.CreateCDataSection(source);
                         nodeSource.AppendChild(cdata);
                     }
 
                     {
                         XmlNode nodeTranslated = this.GetNode(nodeMessage, "translated", true);
-                        XmlNode cdata = this.mDocument.CreateCDataSection(valueDefault);
+                        XmlCDataSection cdata = this.mDocument.CreateCDataSection(source);
                         nodeTranslated.AppendChild(cdata);
                     }
 
                     if (comment != String.Empty)
                     {
                         XmlNode nodeComment = this.GetNode(nodeMessage, "comment", true);
-                        XmlNode cdata = this.mDocument.CreateCDataSection(comment);
+                        XmlCDataSection cdata = this.mDocument.CreateCDataSection(comment);
                         nodeComment.AppendChild(cdata);
                     }
 
                     this.mChanged = true;
 
-                    return valueDefault;
+                    return source;
+                }
+            }
+
+            internal void SetTranslation(string id, string methodName, string source, string translation, string comment)
+            {
+                XmlNode nodeMessage = this.mDocument.DocumentElement.SelectSingleNode(
+                    String.Format("message[id={0}]", id));
+
+                if (nodeMessage != null)
+                {
+                    XmlNode nodeTranslated = this.GetNode(nodeMessage, "translated", true);
+                    nodeTranslated.RemoveAll();
+                    XmlNode cdata = this.mDocument.CreateCDataSection(translation);
+                    nodeTranslated.AppendChild(cdata);
+                    this.mChanged = true;
+                }
+                else
+                {
+                    nodeMessage = this.mDocument.DocumentElement.AppendChild(this.mDocument.CreateElement("message"));
+
+                    {
+                        XmlNode nodeId = this.GetNode(nodeMessage, "id", true);
+                        nodeId.InnerText = id;
+                    }
+
+                    {
+                        XmlNode nodeSource = this.GetNode(nodeMessage, "source", true);
+                        nodeSource.RemoveAll();
+                        XmlNode cdata = this.mDocument.CreateCDataSection(source);
+                        nodeSource.AppendChild(cdata);
+                    }
+
+                    {
+                        XmlNode nodeTranslated = this.GetNode(nodeMessage, "translated", true);
+                        nodeTranslated.RemoveAll();
+                        XmlNode cdata = this.mDocument.CreateCDataSection(translation);
+                        nodeTranslated.AppendChild(cdata);
+                    }
+
+                    if (comment != String.Empty)
+                    {
+                        XmlNode nodeComment = this.GetNode(nodeMessage, "comment", true);
+                        nodeComment.RemoveAll();
+                        XmlNode cdata = this.mDocument.CreateCDataSection(comment);
+                        nodeComment.AppendChild(cdata);
+                    }
+
+                    this.mChanged = true;
                 }
             }
 
