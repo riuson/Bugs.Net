46faef18e91fe1e6d698781cf48c6a98d7035840
Vladimir <riuson@gmail.com>
2015-09-20 23:33:19 +0500

    Added control to select language

--- BugTracker.Translator/Classes/Plugin.cs
+++ BugTracker.Translator/Classes/Plugin.cs
@@ -1,8 +1,10 @@
 ﻿using BugTracker.Core.Classes;
 using BugTracker.Core.Extensions;
 using BugTracker.Core.Interfaces;
+using BugTracker.Translator.Controls;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,12 +32,48 @@ namespace BugTracker.Translator.Classes
                             BugTracker.Translator.Properties.Resources.icon_language_008000_48);
                         menuItemSettings.Click += delegate(object sender, EventArgs ea)
                         {
+                            this.ShowLanguageSelect();
                         };
 
                         return new IButton[] { menuItemSettings };
                     }
                 default:
                     return new IButton[] { };
+            }
+        }
+
+        private void ShowLanguageSelect()
+        {
+            ControlLanguage control = new ControlLanguage(this.mApp);
+            control.Confirmed += this.control_Confirmed;
+            control.Rejected += this.control_Rejected;
+            this.mApp.Controls.Show(control);
+        }
+
+        private void control_Confirmed(object sender, EventArgs e)
+        {
+            ControlLanguage control = sender as ControlLanguage;
+
+            if (control != null)
+            {
+                CultureInfo selectedCulture = control.SelectedCulture;
+
+                if (selectedCulture != null)
+                {
+                    this.mApp.Localization.ActiveUICulture = selectedCulture;
+                }
+
+                this.mApp.Controls.Hide(control);
+            }
+        }
+
+        private void control_Rejected(object sender, EventArgs e)
+        {
+            ControlLanguage control = sender as ControlLanguage;
+
+            if (control != null)
+            {
+                this.mApp.Controls.Hide(control);
             }
         }
     }
