2fb43bda73f3f07d5dad171c5dac1bb0b2ae3882
Vladimir <riuson@gmail.com>
2015-10-12 22:48:45 +0500

    Added project for updater executable

---
+++ Updater/Classes/Plugin.cs
@@ -0,0 +1,52 @@
+﻿using AppCore;
+using AppCore.Classes;
+using AppCore.Extensions;
+using AppCore.Menus;
+using AppCore.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Updater.Classes
+{
+    internal class Plugin : IPlugin
+    {
+        private IApplication mApp;
+
+        public void Initialize(IApplication app)
+        {
+            this.mApp = app;
+        }
+
+        public IButton[] GetCommandLinks(string tag)
+        {
+            switch (tag)
+            {
+                case "settings":
+                    {
+                        IButton menuItemUpdate = MenuPanelFabric.CreateMenuItem("Update".Tr(), "Update application".Tr(), Updater.Properties.Resources.icon_fa_refresh_48_0_005719_none);
+                        menuItemUpdate.Click += delegate(object sender, EventArgs ea)
+                        {
+                            //ControlAbout controlAbout = new ControlAbout(this.mApp);
+                            //this.mApp.Controls.Show(controlAbout);
+                        };
+
+                        return new IButton[] { menuItemUpdate };
+                    }
+                default:
+                    return new IButton[] { };
+            }
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Shutdown()
+        {
+        }
+    }
+}
---
+++ Updater/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Updater")]
+[assembly: AssemblyDescription("Updater utility")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("43d08157-5e7a-4aed-a705-612a66a1d844")]
+
+[assembly: AppCore.Plugins.AssemblyPluginType(typeof(Updater.Classes.Plugin))]
