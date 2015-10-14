5c288490d68ea06b5718e329a48c9286e6e47ab8
Vladimir <riuson@gmail.com>
2015-10-14 18:07:34 +0500

    Added plugin project for updater from filesystem

---
+++ Updater.FileSystem/Classes/Plugin.cs
@@ -0,0 +1,53 @@
+﻿using AppCore;
+using AppCore.Classes;
+using AppCore.Extensions;
+using AppCore.Menus;
+using AppCore.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Updater.FileSystem.Classes
+{
+    internal class Plugin : IPlugin
+    {
+        private IApplication mApp;
+
+        public void Initialize(IApplication app)
+        {
+            this.mApp = app;
+        }
+
+        public IButton[] GetCommandLinks(string tag)
+        {
+            switch (tag)
+            {
+                case "updater":
+                    {
+                        IButton menuItemUpdate = MenuPanelFabric.CreateMenuItem("File system".Tr(), "Setup update from file system".Tr(),
+                            Updater.FileSystem.Properties.Resources.icon_fa_files_o_48_0_005719_none);
+                        menuItemUpdate.Click += delegate(object sender, EventArgs ea)
+                        {
+                            //ControlAbout controlAbout = new ControlAbout(this.mApp);
+                            //this.mApp.Controls.Show(controlAbout);
+                        };
+
+                        return new IButton[] { menuItemUpdate };
+                    }
+                default:
+                    return new IButton[] { };
+            }
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Shutdown()
+        {
+        }
+    }
+}
---
+++ Updater.FileSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Updater.FileSystem")]
+[assembly: AssemblyDescription("Plugin for update application from filesystem")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ee62c6f2-6373-48de-9fb7-3464fe7ca3ce")]
+
+[assembly: AppCore.Plugins.AssemblyPluginType(typeof(Updater.FileSystem.Classes.Plugin))]
