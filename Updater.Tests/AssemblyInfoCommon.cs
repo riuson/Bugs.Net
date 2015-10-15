f606582f887c1a540da39ecf96e5aac34f5eee1b
Vladimir <riuson@gmail.com>
2015-10-15 23:57:20 +0500

    Added test

---
+++ Updater.Tests/AssemblyInfoCommon.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("riuson")]
+[assembly: AssemblyProduct("Bugs.Net")]
+[assembly: AssemblyCopyright("© 2015 riuson")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyFileVersion("1.0.*")]
+
+[AttributeUsage(AttributeTargets.Assembly)]
+internal class AssemblyGitRevisionAttribute : Attribute
+{
+    public string RevisionHash { get; private set; }
+
+    public AssemblyGitRevisionAttribute() : this(string.Empty) { }
+    public AssemblyGitRevisionAttribute(string value) { this.RevisionHash = value; }
+}
+
+[AttributeUsage(AttributeTargets.Assembly)]
+internal class AssemblyGitCommitAuthorDateAttribute : Attribute
+{
+    public DateTime CommitAuthorDate { get; private set; }
+
+    public AssemblyGitCommitAuthorDateAttribute() : this(string.Empty) { }
+    public AssemblyGitCommitAuthorDateAttribute(string value)
+    {
+        DateTime dt;
+
+        if (DateTime.TryParse(value, out dt))
+        {
+            this.CommitAuthorDate = dt;
+        }
+        else
+        {
+            this.CommitAuthorDate = new DateTime(1970, 1, 1, 0, 0, 0);
+        }
+    }
+}
---
+++ Updater.Tests/BArchive/PackerTest.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Updater.BArchive;
+
+namespace Updater.Tests.BArchive
+{
+    [TestFixture]
+    internal class PackerTest
+    {
+        [Test]
+        public void CanPack()
+        {
+            string path = this.GetThisDirectory();
+            path = Path.GetDirectoryName(path);
+            this.Log("Source path: " + path);
+
+            string temp = Path.Combine(Path.GetTempPath(), "packer.barc");
+            this.Log("Output file: " + temp);
+
+            using (FileStream fs = new FileStream(temp, FileMode.Create, FileAccess.ReadWrite))
+            {
+                using (Packer packer = new Packer(fs, this.Log))
+                {
+                    packer.BaseDirectory = path;
+                    packer.AddDirectory(path);
+                }
+            }
+        }
+
+        private string GetThisDirectory()
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            path = Path.GetDirectoryName(path);
+            return path;
+        }
+
+        private void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
