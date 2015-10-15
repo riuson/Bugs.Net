83199c008a2bd5608870f9baa407363a615c13b3
Vladimir <riuson@gmail.com>
2015-10-15 21:09:00 +0500

    Added empty Packer class

---
+++ Updater/BArchive/Packer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Updater.BArchive
+{
+    public class Packer : IDisposable
+    {
+        private Stream mStreamOut;
+
+        public Packer(Stream streamOut)
+        {
+            this.mStreamOut = streamOut;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
