d63358ec5f04be325af6792269ba846a59b2910d
Vladimir <riuson@gmail.com>
2015-10-28 23:53:42 +0500

    Select files with filter and checkboxes

--- Updater/Archive/ControlArchive.Designer.cs
+++ Updater/Archive/ControlArchive.Designer.cs
@@ -28,18 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonSaveArchive = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvFiles = new System.Windows.Forms.DataGridView();
+            this.columnIncluded = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.columnPath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBoxFilter = new System.Windows.Forms.TextBox();
+            this.checkBoxSelecteed = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).BeginInit();
             this.SuspendLayout();
+            // 
+            // buttonSaveArchive
+            // 
+            this.buttonSaveArchive.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSaveArchive.AutoSize = true;
+            this.buttonSaveArchive.Location = new System.Drawing.Point(305, 29);
+            this.buttonSaveArchive.Name = "buttonSaveArchive";
+            this.buttonSaveArchive.Size = new System.Drawing.Size(89, 23);
+            this.buttonSaveArchive.TabIndex = 0;
+            this.buttonSaveArchive.Text = "Save archive...";
+            this.buttonSaveArchive.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.dgvFiles, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxFilter, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSaveArchive, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.checkBoxSelecteed, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(397, 268);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // dgvFiles
+            // 
+            this.dgvFiles.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgvFiles.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFiles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnIncluded,
+            this.columnPath});
+            this.dgvFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvFiles.Location = new System.Drawing.Point(3, 29);
+            this.dgvFiles.Name = "dgvFiles";
+            this.dgvFiles.Size = new System.Drawing.Size(296, 236);
+            this.dgvFiles.TabIndex = 1;
+            this.dgvFiles.SelectionChanged += new System.EventHandler(this.dgvFiles_SelectionChanged);
+            // 
+            // columnIncluded
+            // 
+            this.columnIncluded.DataPropertyName = "Included";
+            this.columnIncluded.HeaderText = "Included";
+            this.columnIncluded.Name = "columnIncluded";
+            // 
+            // columnPath
+            // 
+            this.columnPath.DataPropertyName = "RelativePath";
+            this.columnPath.HeaderText = "Path";
+            this.columnPath.Name = "columnPath";
+            // 
+            // textBoxFilter
+            // 
+            this.textBoxFilter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFilter.Location = new System.Drawing.Point(3, 3);
+            this.textBoxFilter.Name = "textBoxFilter";
+            this.textBoxFilter.Size = new System.Drawing.Size(296, 20);
+            this.textBoxFilter.TabIndex = 2;
+            this.textBoxFilter.TextChanged += new System.EventHandler(this.textBoxFilter_TextChanged);
+            // 
+            // checkBoxSelecteed
+            // 
+            this.checkBoxSelecteed.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.checkBoxSelecteed.AutoSize = true;
+            this.checkBoxSelecteed.Location = new System.Drawing.Point(305, 4);
+            this.checkBoxSelecteed.Name = "checkBoxSelecteed";
+            this.checkBoxSelecteed.Size = new System.Drawing.Size(68, 17);
+            this.checkBoxSelecteed.TabIndex = 3;
+            this.checkBoxSelecteed.Text = "Selected";
+            this.checkBoxSelecteed.UseVisualStyleBackColor = true;
+            this.checkBoxSelecteed.CheckStateChanged += new System.EventHandler(this.checkBoxSelecteed_CheckStateChanged);
             // 
             // ControlArchive
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "ControlArchive";
             this.Size = new System.Drawing.Size(397, 268);
+            this.Load += new System.EventHandler(this.ControlArchive_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonSaveArchive;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dgvFiles;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn columnIncluded;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnPath;
+        private System.Windows.Forms.TextBox textBoxFilter;
+        private System.Windows.Forms.CheckBox checkBoxSelecteed;
     }
 }
--- Updater/Archive/ControlArchive.cs
+++ Updater/Archive/ControlArchive.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -14,13 +15,140 @@ namespace Updater.Archive
     public partial class ControlArchive : UserControl
     {
         private IApplication mApp;
+        private IEnumerable<FileData> mFiles;
+        private BindingSource mBS;
+        private bool mCheckedByCodeFlag;
 
         public ControlArchive(IApplication app)
         {
+            this.mCheckedByCodeFlag = true;
             InitializeComponent();
             this.Text = "Make archive".Tr();
+            this.buttonSaveArchive.Text = this.buttonSaveArchive.Text.Tr();
+            this.columnIncluded.HeaderText = this.columnIncluded.HeaderText.Tr();
+            this.columnPath.HeaderText = this.columnPath.HeaderText.Tr();
 
             this.mApp = app;
+
+            DirectoryInfo rootDirectory = new DirectoryInfo(this.mApp.StartInfo.ExecutableDir);
+            this.mFiles = (from item in rootDirectory.GetFiles("*.*", SearchOption.AllDirectories)
+                           orderby item.FullName
+                           select new FileData(rootDirectory, item)).ToList();
+            this.mBS = new BindingSource();
+            this.mBS.DataSource = this.mFiles;
+            this.dgvFiles.AutoGenerateColumns = false;
+            this.dgvFiles.DataSource = this.mBS;
+        }
+
+        private void ControlArchive_Load(object sender, EventArgs e)
+        {
+            this.columnIncluded.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            this.columnPath.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            this.mCheckedByCodeFlag = false;
+        }
+
+        private void textBoxFilter_TextChanged(object sender, EventArgs e)
+        {
+            if (this.textBoxFilter.Text == String.Empty)
+            {
+                this.mBS.DataSource = this.mFiles;
+            }
+            else
+            {
+                string filter = this.textBoxFilter.Text.ToLower();
+                this.mBS.DataSource = from item in this.mFiles
+                                      where item.RelativePath.ToLower().Contains(filter)
+                                      select item;
+            }
+        }
+
+        private IEnumerable<FileData> GetSelectedFiles()
+        {
+            var files = this.dgvFiles.SelectedCells.OfType<DataGridViewCell>()
+                .Select(item => item.OwningRow)
+                .Distinct()
+                .Where(item => item.DataBoundItem is FileData)
+                .Select(item => item.DataBoundItem as FileData);
+
+            return files;
+        }
+
+        private void dgvFiles_SelectionChanged(object sender, EventArgs e)
+        {
+            var files = this.GetSelectedFiles();
+
+            this.mCheckedByCodeFlag = true;
+
+            if (files.All(item => item.Included))
+            {
+                this.checkBoxSelecteed.CheckState = CheckState.Checked;
+            }
+            else if (!files.Any(item => item.Included))
+            {
+                this.checkBoxSelecteed.CheckState = CheckState.Unchecked;
+            }
+            else
+            {
+                this.checkBoxSelecteed.CheckState = CheckState.Indeterminate;
+            }
+
+            this.mCheckedByCodeFlag = false;
+        }
+
+        private void checkBoxSelecteed_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (!this.mCheckedByCodeFlag)
+            {
+                var files = this.GetSelectedFiles();
+
+                bool chk = this.checkBoxSelecteed.Checked;
+
+                foreach (var item in files)
+                {
+                    item.Included = chk;
+                }
+
+                this.dgvFiles.InvalidateColumn(0);
+            }
+        }
+
+        private class FileData
+        {
+            public FileInfo File { get; private set; }
+            public DirectoryInfo RootDirectory { get; private set; }
+            public bool Included
+            {
+                get
+                {
+                    return this.mIncluded;
+                }
+                set
+                {
+                    this.mIncluded = value;
+                }
+            }
+            private bool mIncluded;
+
+            public FileData(DirectoryInfo rootDirectory, FileInfo file)
+            {
+                this.RootDirectory = rootDirectory;
+                this.File = file;
+                this.Included = true;
+            }
+
+            public string RelativePath
+            {
+                get
+                {
+                    string result = this.File.FullName.Replace(this.RootDirectory.FullName, String.Empty);
+                    return result;
+                }
+            }
+
+            public override string ToString()
+            {
+                return this.RelativePath;
+            }
         }
     }
 }
