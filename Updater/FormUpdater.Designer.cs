bc3a7b68f9338b5ce247c5acb295cfaa9ca1c2a6
Vladimir <riuson@gmail.com>
2015-10-29 20:16:55 +0500

    Updater window size changed

--- Updater/FormUpdater.Designer.cs
+++ Updater/FormUpdater.Designer.cs
@@ -38,7 +38,7 @@
             this.richTextBoxLog.Location = new System.Drawing.Point(0, 0);
             this.richTextBoxLog.Name = "richTextBoxLog";
             this.richTextBoxLog.ReadOnly = true;
-            this.richTextBoxLog.Size = new System.Drawing.Size(284, 262);
+            this.richTextBoxLog.Size = new System.Drawing.Size(724, 419);
             this.richTextBoxLog.TabIndex = 0;
             this.richTextBoxLog.Text = "";
             // 
@@ -46,7 +46,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(724, 419);
             this.Controls.Add(this.richTextBoxLog);
             this.Name = "FormUpdater";
             this.Text = "Updater";
